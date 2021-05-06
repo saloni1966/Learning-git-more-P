e7c98a724fde388269f6b2a818210534130469e7
saloni1966 <salonisharma1966@gmail.com>
2021-05-06 21:54:09 +0530

    added 3rd part

--- Story.js
+++ Story.js
@@ -3,4 +3,5 @@ Once upon time there lived crocodile , one day he was very hungry and was restin
 thereafter he saw a monkey eating sweet berries.
 equested him to give some to him.They both became good friends and then it became a regular 
 affair of crocodile to come and join monkey. One day Crocodile requested monkey to give some berries to him for his wife.
+Corcodile's wife started thinking that if the monkey eats berries , his heart might be more sweet.
 
