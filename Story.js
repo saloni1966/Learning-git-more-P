23ff6e8d2d7b8ad354d076993f95842247166930
saloni1966 <salonisharma1966@gmail.com>
2021-05-06 21:46:38 +0530

    added 2nd part

--- Story.js
+++ Story.js
@@ -2,4 +2,5 @@
 Once upon time there lived crocodile , one day he was very hungry and was resting below a tree, 
 thereafter he saw a monkey eating sweet berries.
 equested him to give some to him.They both became good friends and then it became a regular 
-affair of crocodile to come and join monkey. 
+affair of crocodile to come and join monkey. One day Crocodile requested monkey to give some berries to him for his wife.
+
